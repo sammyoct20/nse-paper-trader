0bc9bc17a4f1014da85ff400bee2ce4c5effa110
sammyoct20 <sammyoct20@gmail.com>
2026-08-10 15:31:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
+import time
 from engine import PaperEngine
-import os
 
-def run_engine():
-    PaperEngine().run_loop()
+engine = PaperEngine()
 
-threading.Thread(target=run_engine, daemon=True).start()
-
-os.system("streamlit run app.py --server.port 10000 --server.address 0.0.0.0")
+while True:
+    engine.run_once()
+    time.sleep(300)   # runs every 5 minutes
