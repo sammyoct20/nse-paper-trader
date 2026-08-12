c0b3a926f051810512cd0c393fe41931dd3e5a45
sammyoct20 <sammyoct20@gmail.com>
2026-08-12 11:20:31 +0530

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,35 +5,16 @@ import traceback
 
 def main():
 
-    print("\n==============================")
     print("🚀 Worker Started")
-    print("==============================\n")
-
-    print(f"🕒 Time: {datetime.datetime.now()}\n")
+    print("Time:", datetime.datetime.now())
 
     try:
         engine = PaperEngine()
-
         trades = engine.run_once()
 
-        print(f"📊 Trades Found: {len(trades)}\n")
+        print("Trades Found:", len(trades))
 
-        if not trades:
-            print("❌ No trades (market weak or no setups)\n")
-        else:
-            print("🔥 New Trades:\n")
-
-            for t in trades:
-                print(f"""
-Symbol: {t['symbol']}
-Entry: {t['entry']}
-SL: {t['sl']}
-Target: {t['target']}
----------------------------
-""")
-
-    except Exception as e:
-        print("❌ ERROR OCCURRED\n")
+    except Exception:
         traceback.print_exc()
 
 
