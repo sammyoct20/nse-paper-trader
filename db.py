efe79b9a55cc50030aeb0a3adb4936d634a9f103
sammyoct20 <sammyoct20@gmail.com>
2026-08-17 14:23:00 +0530

    Update db.py

--- db.py
+++ db.py
@@ -2,10 +2,8 @@ import sqlite3
 
 DB_NAME = "trades.db"
 
-
 def get_conn():
     return sqlite3.connect(DB_NAME, check_same_thread=False)
-
 
 def create_tables():
     conn = get_conn()
@@ -22,6 +20,8 @@ def create_tables():
         entry_price REAL,
         exit_price REAL,
         pnl REAL,
+        type TEXT,
+        direction TEXT,
         exit_reason TEXT,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         closed_at TIMESTAMP
